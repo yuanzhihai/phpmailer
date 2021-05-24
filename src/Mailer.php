fabbf1bec002be72467a8db32052bb2ec0328e87
dtid_4cb73ba1d0cd43af <396751927@qq.com>
2021-05-24 13:37:49 +0800

    init

--- src/Mailer.php
+++ src/Mailer.php
@@ -8,18 +8,18 @@ namespace yzh52521\PHPMailer;
  * @package \yzh52521\PHPMailer\Mailer
  * @mixin \yzh52521\PHPMailer\service\Mailer
  * @method static Mailer init() 初始化
- * @method static Mailer setSender($address, $senderName = '') 设置发送人
- * @method static Mailer setReplyAddress($address = '', $replayName = '') 设置收件人回复地址
- * @method static Mailer setAddressee($address, $recName = '') 设置单个接收人
- * @method static Mailer  setManyAddressee($array) 设置多个接收人
- * @method static Mailer  setCC($address = '', $name = '') 设置抄送单个接收人
- * @method static Mailer setManyCC($array) 设置抄送多个接收人
- * @method static Mailer  setBCC($address = '', $name = '') 设置暗抄送单个接收人
- * @method static Mailer setManyBCC($array) 设置暗抄送多个接收人
- * @method static Mailer setAttachment($filename, $name = '') 设置单个附件
- * @method static Mailer setManyAttachment($array) 设置多个附件
- * @method static Mailer  setContent($title = '', $body = '', $altBody = '') 设置邮件内容
- * @method static send() 发送邮件
+ * @method  Mailer setSender($address, $senderName = '') 设置发送人
+ * @method  Mailer setReplyAddress($address = '', $replayName = '') 设置收件人回复地址
+ * @method  Mailer setAddressee($address, $recName = '') 设置单个接收人
+ * @method  Mailer  setManyAddressee($array) 设置多个接收人
+ * @method  Mailer  setCC($address = '', $name = '') 设置抄送单个接收人
+ * @method  Mailer setManyCC($array) 设置抄送多个接收人
+ * @method  Mailer  setBCC($address = '', $name = '') 设置暗抄送单个接收人
+ * @method  Mailer setManyBCC($array) 设置暗抄送多个接收人
+ * @method  Mailer setAttachment($filename, $name = '') 设置单个附件
+ * @method  Mailer setManyAttachment($array) 设置多个附件
+ * @method  Mailer  setContent($title = '', $body = '', $altBody = '') 设置邮件内容
+ * @method  send() 发送邮件
  */
 class Mailer
 {
