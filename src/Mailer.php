c2bac35bfdd4a8936c48e817454c459b18875111
dtid_4cb73ba1d0cd43af <396751927@qq.com>
2021-05-24 14:30:41 +0800

    init

--- src/Mailer.php
+++ src/Mailer.php
@@ -7,19 +7,19 @@ namespace yzh52521\PHPMailer;
  * @see \yzh52521\PHPMailer\Mailer
  * @package \yzh52521\PHPMailer\Mailer
  * @mixin \yzh52521\PHPMailer\service\Mailer
- * @method static Mailer init() 初始化
- * @method  Mailer setSender($address, $senderName = '') 设置发送人
- * @method  Mailer setReplyAddress($address = '', $replayName = '') 设置收件人回复地址
- * @method  Mailer setAddressee($address, $recName = '') 设置单个接收人
- * @method  Mailer  setManyAddressee($array) 设置多个接收人
- * @method  Mailer  setCC($address = '', $name = '') 设置抄送单个接收人
- * @method  Mailer setManyCC($array) 设置抄送多个接收人
- * @method  Mailer  setBCC($address = '', $name = '') 设置暗抄送单个接收人
- * @method  Mailer setManyBCC($array) 设置暗抄送多个接收人
- * @method  Mailer setAttachment($filename, $name = '') 设置单个附件
- * @method  Mailer setManyAttachment($array) 设置多个附件
- * @method  Mailer  setContent($title = '', $body = '', $altBody = '') 设置邮件内容
- * @method  send() 发送邮件
+ * @method static \yzh52521\PHPMailer\service\Mailer init() 初始化
+ * @method static \yzh52521\PHPMailer\service\Mailer setSender($address, $senderName = '') 设置发送人
+ * @method static \yzh52521\PHPMailer\service\Mailer setReplyAddress($address = '', $replayName = '') 设置收件人回复地址
+ * @method static \yzh52521\PHPMailer\service\Mailer setAddressee($address, $recName = '') 设置单个接收人
+ * @method static \yzh52521\PHPMailer\service\Mailer  setManyAddressee($array) 设置多个接收人
+ * @method static \yzh52521\PHPMailer\service\Mailer  setCC($address = '', $name = '') 设置抄送单个接收人
+ * @method static \yzh52521\PHPMailer\service\Mailer setManyCC($array) 设置抄送多个接收人
+ * @method static \yzh52521\PHPMailer\service\Mailer  setBCC($address = '', $name = '') 设置暗抄送单个接收人
+ * @method static \yzh52521\PHPMailer\service\Mailer setManyBCC($array) 设置暗抄送多个接收人
+ * @method static \yzh52521\PHPMailer\service\Mailer setAttachment($filename, $name = '') 设置单个附件
+ * @method static \yzh52521\PHPMailer\service\Mailer setManyAttachment($array) 设置多个附件
+ * @method static \yzh52521\PHPMailer\service\Mailer  setContent($title = '', $body = '', $altBody = '') 设置邮件内容
+ * @method static \yzh52521\PHPMailer\service\Mailer send() 发送邮件
  */
 class Mailer
 {
